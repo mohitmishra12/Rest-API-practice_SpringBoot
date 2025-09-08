67db72a3c2a2cca4799b6f2d202aacaec1c28d74
mohitmishra12 <mohitmishra747041@gmail.com>
2025-09-08 16:24:07 +0530

    Use lombol in our project

--- src/main/java/rest/api/com/example/model/User.java
+++ src/main/java/rest/api/com/example/model/User.java
@@ -1,52 +1,18 @@
 package rest.api.com.example.model;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import lombok.*;
 
 @Entity
-@Table(name = "users")   // optional, table name in DB
+@Table(name = "users")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class User {
-
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Integer id;
+    private Long id;
 
     private String name;
     private String email;
-
-    // default constructor (required by JPA)
-    public User() {
-    }
-
-    // constructor
-    public User(Integer id, String name, String email) {
-        this.id = id;
-        this.name = name;
-        this.email = email;
-    }
-
-    // getters & setters
-    public Integer getId() {
-        return id;
-    }
-    public void setId(Integer id) {
-        this.id = id;
-    }
-
-    public String getName() {
-        return name;
-    }
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getEmail() {
-        return email;
-    }
-    public void setEmail(String email) {
-        this.email = email;
-    }
 }
